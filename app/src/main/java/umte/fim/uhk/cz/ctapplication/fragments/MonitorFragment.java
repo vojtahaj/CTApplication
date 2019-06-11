9b14aee34a7f775aa75e99100a3fd257d880f9d9
vojtahaj <vojta.haj@gmail.com>
2019-06-11 21:02:25 +0200

    Monitor linky
    Zobrazovani na monitor, barevne odliseni, mazani

--- app/src/main/java/umte/fim/uhk/cz/ctapplication/fragments/MonitorFragment.java
+++ app/src/main/java/umte/fim/uhk/cz/ctapplication/fragments/MonitorFragment.java
@@ -1,6 +1,7 @@
 package umte.fim.uhk.cz.ctapplication.fragments;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import umte.fim.uhk.cz.ctapplication.CTActivity;
 import umte.fim.uhk.cz.ctapplication.R;
@@ -21,10 +23,19 @@ public class MonitorFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_monitor,container,false);
+        View view = inflater.inflate(R.layout.fragment_monitor, container, false);
 
         btnClear = view.findViewById(R.id.btnClearMonitor);
         txtMonitor = view.findViewById(R.id.txtMonitor);
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Vymaze jenom textView ne cely arraylist
+                txtMonitor.setText("");
+                //  Toast.makeText(getActivity(), "cistim",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         setMonitor();
 
@@ -33,16 +44,17 @@ public class MonitorFragment extends Fragment {
 
     @SuppressLint("ResourceAsColor")
     private void setMonitor() {
-
+        Toast.makeText(getActivity(), "monitor vypis", Toast.LENGTH_LONG).show();
         if (CTActivity.monitorLogs.size() > 0)
-        for (int i = 0; i < CTActivity.monitorLogs.size(); i++){
-            txtMonitor.setTextColor(R.color.colorOrange);
-            txtMonitor.setText(CTActivity.monitorLogs.get(i).getTime().toString() + ": ");
-            if (CTActivity.monitorLogs.get(i).isRecieved())
-                txtMonitor.setTextColor(R.color.colorBlack);
-            else txtMonitor.setTextColor(R.color.colorGreen);
-            txtMonitor.append(CTActivity.monitorLogs.get(i).getMessage() + "\n");
-        }
+            for (int i = 0; i < CTActivity.monitorLogs.size(); i++) {
+                txtMonitor.setTextColor(Color.YELLOW);
+                txtMonitor.append(CTActivity.monitorLogs.get(i).getTime().toString() + ": ");
+                if (CTActivity.monitorLogs.get(i).isRecieved())
+                    txtMonitor.setTextColor(Color.BLACK);
+                else txtMonitor.setTextColor(Color.GREEN);
+                txtMonitor.append(CTActivity.monitorLogs.get(i).getMessage() + "\n");
+                // System.out.println("Vypisuji do monitooru: " + CTActivity.monitorLogs.size());
+            }
 
     }
 }
